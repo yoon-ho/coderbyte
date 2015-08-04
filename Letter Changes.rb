52d689043438ff8c3acbb46bc87ecbc77f933d3e
Son Yoon Ho(Levi) <yoon-ho@users.noreply.github.com>
2015-08-04 20:19:03 +0900

    Create Letter Changes.rb

---
+++ Letter Changes.rb
@@ -0,0 +1,13 @@
+def LetterChanges(str)
+
+  # code goes here
+  str.tr!('a-z', 'b-za')
+  str.tr!('aeiou', 'AEIOU')
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterChanges(STDIN.gets)           
+
